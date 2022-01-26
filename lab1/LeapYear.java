eda557ab514957882a2d22fd664f376634f5bb34
John <xinhui.jiang@ucdconnect.ie>
2022-01-26 14:25:01 +0000

    Modified LeapYear.java

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -29,5 +29,12 @@ public class LeapYear {
             }
         }
     }
+	
+	/** Check if a year is a leap year */
+	public static boolean isLeapYear(int year) {
+		if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+			return true;
+		return false;
+	}
 }
 
